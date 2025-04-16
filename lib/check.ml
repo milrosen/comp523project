b240ef14da7b1b0f1721f04df60827360cdeca13
Milton Rosenbaum <milton.rosenbaum@gmail.com>
2025-04-15 22:24:53 -0400

    finished macro type

--- lib/check.ml
+++ lib/check.ml
@@ -8,10 +8,10 @@ exception TypeError of string
   macro definitions in order, building up the Gamma and Phi 
   contexts, as well as the rho substitution environment *)
 let initial_ctx = Context.empty
-  |> Context.set Macro "lambda" (S.Arrow (List [List[Ident]; Type Expr], Expr))
+  |> Context.set Macro "lambda" (S.Arrow (List [List [Ident]; Type Expr], Expr))
   |> Context.set Macro "define" (S.Arrow (List [Ident; Type Expr], Def))
-  |> Context.set Macro "quote"  (S.Arrow (List[Any], Expr))
-  |> Context.set Macro "app"    (S.Arrow (List[Type Expr; Type Expr], Expr))
+  |> Context.set Macro "quote"  (S.Arrow (List [Any], Expr))
+  |> Context.set Macro "app"    (S.Arrow (List [Type Expr; Type Expr], Expr))
 
 let check_program ctx env ast = raise (TypeError "not implemented yet")
 
@@ -21,32 +21,71 @@ let rec symbol_to_type s =
   | "expr" -> S.Expr
   | t -> raise (TypeError ("type " ^ t ^ " does not exist"))
 
-let rec guarded = raise (TypeError "Not Implemented")
-let rec unguarded = raise (TypeError "Not Implemented")
+let rec sexpr_to_shape s =
+  match s with
+  | A.List l -> S.List (List.map sexpr_to_shape l)
+  | A.Symbol s -> try S.Type (symbol_to_type s) with 
+    | TypeError _ -> raise (TypeError ("invalid type " ^ s ^ " in guard clauses"))
 
-let macro_type ctx env clauses t = 
-  let t = symbol_to_type t in
+(* in the paper, it seems that the context can contain
+   way more than just pattern variables, it can also contain 
+   named abbreviations and the dot (a b . (c d)) = (a b c d) 
+   however, strict to the studs, it seems that neither of
+   these are allowed in the judgements they actually define *)
+let rec lookup_pvar pvar guard = 
+  match guard with 
+  | A.List (A.List [A.Symbol var; s] :: _) when var = pvar -> sexpr_to_shape s
+  | A.List (A.List [A.Symbol _; _] :: rst) -> lookup_pvar pvar (A.List rst)  
+  | _ -> raise (TypeError "guard clauses malformed")
+
+let guarded _ctx guard pattern macro = 
+  let rec go pattern =
+    (match pattern with 
+    | A.List [] -> S.List []
+    | A.List l -> S.List (List.map go l)
+    | A.Symbol pvar -> lookup_pvar pvar guard)
+  in
+  match pattern with 
+  | A.List [A.Symbol m ; pattern] when m = macro -> go pattern
+  | _ -> raise (TypeError "macro clauses must begin with the name of the macro")
+
+let unguarded pattern macro = 
+  let rec go pattern =
+    (match pattern with 
+    | A.List [] -> S.List []
+    | A.List l -> S.List (List.map go l)
+    | A.Symbol _ -> S.Any)
+  in 
+  match pattern with 
+  | A.List [A.Symbol m; pattern] when m = macro -> go pattern
+  | _ -> raise (TypeError "macro clauses must begin with the name of the macro")
+
+let rec templates = failwith "Not Implemented"
+
+(* in a  *)
+let macro_type ctx clauses t macro = 
   let rec go clauses acc =
   match clauses with 
   | [] -> if S.no_overlap acc then S.Arrow (Mclauses acc, t) else raise (TypeError "macro contains overlapping clauses" )
-  | A.List [pattern; guards; _] :: clauses ->
-    let g = guarded guards pattern in
-    let u = unguarded pattern in
+  | A.List [pattern; guard] :: clauses ->
+    let g = guarded ctx guard pattern macro in
+    let u = unguarded pattern macro in
       go clauses ((g, u) :: acc)
   | _ -> raise (TypeError "malformed macro")
   in go clauses []
 
- let check_macros ast =
-  let rec go ctx env ast = 
+let check_macros ast =
+  let rec go ctx subst_env ast = 
     match ast with
     | A.List [Symbol "define-syntax"; Symbol macro;
             List[Symbol "syntax-laws"; Symbol t; 
-              List macro_clauses]] :: ast ->
-      let env = Env.add macro (macro_clauses, t) env in
-      let ctx = Context.set Macro macro (macro_type env ctx macro_clauses t) ctx in
-      go ctx env ast
-    | _ -> check_program ctx env ast
-in go initial_ctx Env.empty ast 
+              List clauses]] :: ast ->
+      let t = symbol_to_type t in
+      let subst_env = Env.add macro (clauses, t) subst_env in
+      let ctx = Context.set Macro macro (macro_type ctx clauses t macro) ctx in
+      go ctx subst_env ast
+    | _ -> check_program ctx subst_env ast
+  in go initial_ctx Env.empty ast 
 
 
 (* Phase two: checking the rest of the program as well as
@@ -55,5 +94,6 @@ in go initial_ctx Env.empty ast
   and phi, and then see if the macros are possible to expand *)
 (* I believe that the output of this section should be the "typed"
   AST written in the Surface language *)
+
 
 
--- lib/context.ml
+++ lib/context.ml
@@ -10,8 +10,8 @@ type env = {
     phi : S.s Data.t;
 }
 
-let empty = { gamma = Data.empty; phi = Data.empty}
 
+let empty = { gamma = Data.empty; phi = Data.empty}
 let check_against key ctx =
     match Data.find_opt key ctx with 
     | None -> ()
@@ -21,8 +21,7 @@ let update_ctx key value ctx =
     match Data.find_opt key ctx with 
     | None -> Data.add key value ctx
     | Some s -> assert S.(value <= s) ; Data.add key value ctx
-
-let set vartype  key value {gamma ; phi} =
+let set vartype key value {gamma ; phi} =
     if vartype = Macro then 
         (check_against key phi ; 
         {gamma=update_ctx key value gamma;phi=phi})
